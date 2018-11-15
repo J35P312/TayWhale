fa77bf5937a562b48cfa7bae7f446689d1eaedae
J35P312 <jesper.eisfeldt@scilifelab.se>
2018-11-15 20:07:22 +0100

    modified:   TayWhale.nf 	modified:   install.py 	modified:   template_conf/FindSV.config

--- TayWhale.nf
+++ TayWhale.nf
@@ -53,7 +53,7 @@ if(params.help){
             file "${params.sample}.sf" into salmon_quant
 
         """
-        salmon quant --index ${params.STAR_ref_dir}/cDNA_seqs_index --libType ${params.libtype} --output salmon_output -1 <(bunzip2 ${r1_salmon}) -2 <(bunzip2 ${r2_salmon})
+        salmon quant --index ${params.salmon_index} --libType ${params.libtype} --output salmon_output -1 <(bunzip2 ${r1_salmon}) -2 <(bunzip2 ${r2_salmon})
         mv salmon_output/quant.sf ${params.sample}.sf
         """
     } 
@@ -75,8 +75,7 @@ if(params.help){
 
         """
         
-        STAR --genomeDir ${params.STAR_ref_dir} --readFilesIn ${r1} ${r2}  --twopassMode Basic --outReadsUnmapped None --chimSegmentMin 12 --chimJunctionOverhangMin 12 --alignSJDBoverhangMin 10 --alignMatesGapMax 100000 --alignIntronMax 100000 --chimSegmentReadGapMax parameter 3 --alignSJstitchMismatchNmax 5 -1 5 5 --runThreadN 16 --limitBAMsortRAM 31532137230 --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ${params.sample}. --quantMode GeneCounts --outSAMstrandField intronMotif --readFilesCommand gunzip -c
-
+        STAR --genomeDir ${params.STAR_ref_dir} --readFilesIn <(bunzip2 ${r1}) <(bunzip2 ${r2})  --twopassMode Basic --outReadsUnmapped None --chimSegmentMin 12 --chimJunctionOverhangMin 12 --alignSJDBoverhangMin 10 --alignMatesGapMax 100000 --alignIntronMax 100000 --chimSegmentReadGapMax parameter 3 --alignSJstitchMismatchNmax 5 -1 5 5 --runThreadN 16 --limitBAMsortRAM 31532137230 --outSAMtype BAM SortedByCoordinate --outFileNamePrefix ${params.sample}. --quantMode GeneCounts --outSAMstrandField intronMotif --readFilesCommand gunzip -c
         picard AddOrReplaceReadGroups I= ${params.sample}.Aligned.sortedByCoord.out.bam  O= ${params.sample}.RG.Aligned.sortedByCoord.out.bam RGLB=${params.rglb} RGPL=${params.rgpl} RGPU=${params.rgpu} RGSM=${params.sample}
         rm ${params.sample}.Aligned.sortedByCoord.out.bam
 
--- install.py
+++ install.py
@@ -2,37 +2,11 @@ import sys
 import  os
 
 programDirectory = os.path.dirname(os.path.abspath(__file__))
-os.system("mkdir {}".format(sys.argv[3]))
-os.system("cp {} {}/genome.fa".format(sys.argv[1],sys.argv[3]))
-os.system("cp {} {}/transcripts.GTF".format(sys.argv[2],sys.argv[3]))
-
-os.system("STAR --runThreadN 16 --runMode genomeGenerate --genomeDir {} --genomeFastaFiles {}/genome.fa --sjdbGTFfile {} --sjdbOverhang {}".format(sys.argv[3],sys.argv[3],sys.argv[2],sys.argv[4]))
-
-os.system("samtools faidx {}/genome.fa".format(sys.argv[3]))
-
-os.system("picard CreateSequenceDictionary R=$3/genome.fa".format(sys.argv[3]))
-
-os.system("wget https://github.com/FusionFilter/FusionFilter/archive/FusionFilterv0.4.0.zip")
-os.system("unzip FusionFilterv0.4.0.zip")
-os.system("./FusionFilter-FusionFilterv0.4.0/util/gtf_file_to_feature_seqs.pl  {}/transcripts.GTF {}/genome.fa cDNA > {}/cDNA_seqs.fa".format(sys.argv[3],sys.argv[3],sys.argv[3]))
-os.system("makeblastdb -in {}/cDNA_seqs.fa -dbtype nucl".format(sys.argv[3]))
-
-os.system( "salmon index --index {}/cDNA_seqs_index --transcripts {}/cDNA_seqs.fa".format( sys.argv[3],sys.argv[3] ) )
-
-os.system("blastn -query {}/cDNA_seqs.fa -db {}/cDNA_seqs.fa -max_target_seqs 10000 -outfmt 6 -evalue 1e-3 -lcase_masking -num_threads 16 -word_size 11  >  {}/blast_pairs.outfmt6".format(sys.argv[3],sys.argv[3],sys.argv[3]))
-os.system("./FusionFilter-FusionFilterv0.4.0/prep_genome_lib.pl --genome_fa {}/genome.fa --gtf {}/transcripts.GTF --blast_pairs {}/blast_pairs.gene_syms.outfmt6.gz --cpu 16 ---output_dir {}/".format(sys.argv[3],sys.argv[3],sys.argv[3],sys.argv[3]))
-
 template=""
 for line in open("template_conf/FindSV.config"):
     template += line
 
-template=template.replace("ref=\"\"","ref={}/genome.fa".format(sys.argv[3]))
-
-template=template.replace("STAR_ref_dir=\"\"","STAR_ref_dir={}".format(sys.argv[3]))
-
-template=template.replace("ctat_folder=\"\"","ctat_folder={}".format(sys.argv[3]))
-
-template=template.replace("BootstrapAnn=","BootstrapAnn={}/BootstrapAnn/BootstrapAnn.py".format(programDirectory))
+template=template.replace("BootstrapAnn=\"\"","BootstrapAnn=\"{}/BootstrapAnn/BootstrapAnn.py\"".format(programDirectory))
 
 print template
 
